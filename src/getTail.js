9647924d637daf48624b7b7d78b8e276b3804c9d
Tushar Mathur <tusharmath@gmail.com>
2016-03-12 15:17:07 +0530

    perf(getTail): add getTail()

---
+++ src/getTail.js
@@ -0,0 +1,13 @@
+const Rx = require('rx')
+const u = require('./Utils')
+
+/**
+ * Returns the tail part ie. 100% and then back to 0%
+ *
+ * @param {Scheduler} scheduler - should be Rx.Scheduler.requestAnimationFrame
+ * @param {Observable} source - The source stream which emits boolean values
+ * @returns {Observable}
+ */
+module.exports = (scheduler, source) => u
+    .getStop(source)
+    .flatMap(() => Rx.Observable.from([100, 0], (x) => x, null, scheduler))
---
+++ test/test.getTail.js
@@ -0,0 +1,23 @@
+import test from 'ava'
+import { TestScheduler, ReactiveTest } from 'rx'
+import getTail from '../src/getTail'
+const {onNext} = ReactiveTest
+
+const testSubscriber = x => {
+  const out = []
+  x.subscribe(x => out.push(x))
+  return out
+}
+
+test(t => {
+  const sh = new TestScheduler()
+  const source = sh.createHotObservable(
+    onNext(210, true),
+    onNext(220, false),
+    onNext(230, true),
+    onNext(240, false)
+  )
+  const out = testSubscriber(getTail(sh, source))
+  sh.start()
+  t.same(out, [100, 0, 100, 0])
+})
