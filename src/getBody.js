06162c92d96f742dbd277fb032fa427086f0951f
Tushar Mathur <tusharmath@gmail.com>
2016-03-12 15:46:13 +0530

    style(getBody): add jsDoc comments

--- src/getBody.js
+++ src/getBody.js
@@ -1,5 +1,11 @@
 const e = require('./Utils')
-
+/**
+ * Dispatches a value between 0 - 100, never actually reaching 100.
+ *
+ * @param {Observable} source - The input source determining when to start/stop
+ * @param {Function} sigmoid - The custom sigmoid function that converts an number into a value between 0 & 100.
+ * @returns {Observable}
+ */
 module.exports = (source, sigmoid, getAnimationFrames) => {
   const start = e.getStart(source)
   const stop = e.getStop(source)
