f79e20282025453b8225078e40921660578ce958
Tushar Mathur <tusharmath@gmail.com>
2016-03-12 20:37:43 +0530

    test(index): add basic require test

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-const Rx = require('rx')
-const RxDOM = require('rx-dom')
-const e = exports
+const merge = require('./src/merge')
+const getBody = require('./src/getBody')
+const getTail = require('./src/getTail')
+const scheduler = require('rx-dom').Scheduler.requestAnimationFrame
 
-e.getAnimationFrames = () => Rx
-    .Observable
-    .generate(
-      0, () => true, x => x + 1, x => x,
-      RxDOM.Scheduler.requestAnimationFrame
-)
+const factory = () => {
 
-e.getStart = (source) => source.filter(x => Boolean(x))
-e.getStop = (source) => source.filter(x => !Boolean(x))
-e.getTail = () => Rx.Observable.from(
-    [100, 0],
-    (x) => x, null, RxDOM.Scheduler.requestAnimationFrame
-)
-e.tail = (source) => e.start(source).flatMap(() => e.createTail())
-
-e.sigmoid = (x, speed) => (2 / (1 + Math.pow(2, -x * speed)) - 1) * 100
-
-e.body = (e, source, speed) => {
-  const start = e.start(source)
-  const stop = e.stop(source)
-  return start
-    .flatMap(() => e.getAnimationFrames())
-    .map(x => e.sigmoid(x, speed))
-    .takeUntil(stop)
 }
 
-e.create = (e, source, speed) => {
-  const head = Rx.Observable.return(0)
-  const body = e.body(source, speed)
-  const tail = e.tail(source)
-
-  return Rx.Observable.merge(head, body, tail)
-}
+exports.create = (source, speed) => factory(source, speed)
