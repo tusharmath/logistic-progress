aeab4d5eccd04bf2a84a97b78a699acc726a5667
Tushar Mathur <tusharmath@gmail.com>
2016-03-13 20:38:32 +0530

    style(all): whitespace changes

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ e.getStop = (source) => source.filter(x => !Boolean(x))
 
 e.getAnimationFrames = partialize((scheduler) => Rx
     .Observable
-    .generate(0, () => true, x => x + 1, x => x, scheduler
-), RequestAnimationFrame)
+    .generate(0, () => true, x => x + 1, x => x, scheduler)
+  , RequestAnimationFrame)
 
 e.getBody = partialize((sigmoid, getAnimationFrames, source, rate, initialValue) => {
   const start = e.getStart(source)
@@ -24,20 +24,23 @@ e.getBody = partialize((sigmoid, getAnimationFrames, source, rate, initialValue)
   return start
     .flatMap(() => getAnimationFrames().takeUntil(stop))
     .map(x => sigmoid(x + initialValue, rate))
-}, e.sigmoid, e.getAnimationFrames)
+}
+  , e.sigmoid, e.getAnimationFrames)
 
 e.getHead = () => Rx.Observable.just(0)
 
-e.getTail = partialize((scheduler, source) => e
+e.getTail = partialize((scheduler, source, timeout) => e
     .getStop(source)
-    .flatMap(() => Rx.Observable.from([100, 0], (x) => x, null, scheduler)), RequestAnimationFrame)
+    .flatMap(() => Rx.Observable.from([100, 0], (x) => x, null, scheduler))
+  , RequestAnimationFrame)
 
 e.merge = partialize((getHead, getBody, getTail, source, options) => {
   const head = getHead()
   const body = getBody(source, options.rate, options.start)
   const tail = getTail(source)
   return Rx.Observable.merge(head, body, tail)
-}, e.getHead, e.getBody, e.getTail)
+}
+  , e.getHead, e.getBody, e.getTail)
 
 exports.create = (source, options) => injector
     .call(e.merge, source, Object.assign({}, DEFAULTS, options))
